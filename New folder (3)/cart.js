f7b1714021c17f55ce9d8e0c2499603fc127e806
aakashsingh0044-create <aakashsingh0044@gmail.com>
2026-06-27 00:49:09 +0530

    Update cart.js

--- New folder (3)/cart.js
+++ New folder (3)/cart.js
@@ -7,3 +7,31 @@ function getCart() {
 function saveCart(cart) {
     localStorage.setItem("cart", JSON.stringify(cart));
 }
+
+
+function addToCart(productId) {
+
+    let cart = getCart();
+
+    const product = products.find(p => p.id === productId);
+
+    if (!product) return;
+
+    const existingItem = cart.find(item => item.id === productId);
+
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.push({
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            image: product.image,
+            quantity: 1
+        });
+    }
+
+    saveCart(cart);
+
+    console.log(cart);
+}
