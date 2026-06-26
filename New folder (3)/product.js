6f527c50c4dd0f82a06e9a41902304c216320e3d
aakashsingh0044-create <aakashsingh0044@gmail.com>
2026-06-27 00:46:11 +0530

    Create product.js

---
+++ New folder (3)/product.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    id: 1,
+    sku: "ADC001",
+    name: "Diamond Concrete Candle",
+    price: 1499,
+    image: "assets/concrete-collection-hero.png",
+    fragrance: "Vanilla Oud",
+    burnTime: "40 Hours",
+    material: "Hand-cast Concrete",
+    wick: "Natural Wood",
+    stock: 25,
+    description: "Handcrafted concrete candle featuring our signature diamond design."
+  }
+];
+
+
